582f60ae5c5dcd8e04588e9b1b64b4bda94638d4
srujankale <77841914+srujankale@users.noreply.github.com>
2021-01-24 13:20:57 +0530

    golemattack

---
+++ GameJam/Assets/golem/boulder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boulder : MonoBehaviour
+{
+    public int force,min,max;
+    public float upthrust,time;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        force = Random.Range(min, max+1);
+        player = GameObject.Find("player");
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(-force,upthrust),ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            Object.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "player" && player.GetComponent<playerhealth>().phase == false)
+        {
+            player.GetComponent<playerhealth>().health -= 1;
+            player.GetComponent<playerhealth>().phase = true;
+        }
+    }
+}
---
+++ GameJam/Assets/golem/firstboss.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstboss : MonoBehaviour
+{
+    public GameObject projectile;
+    public float time, timeval,interval,intervalval,bullets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = timeval;
+        interval = intervalval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(bullets <= 0)
+        {
+            interval = intervalval;
+            bullets = 3;
+        }
+        interval -= Time.deltaTime;
+
+        if (interval <= 0)
+        {
+            time-= Time.deltaTime;
+        }
+
+        if (time <= 0)
+        {
+            Instantiate(projectile,transform.position,Quaternion.identity);
+            time = timeval;
+            bullets--;
+        }
+    }
+}
