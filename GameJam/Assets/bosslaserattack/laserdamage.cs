a9376a07453caf026ce8961f4559f3077238d87c
srujankale <77841914+srujankale@users.noreply.github.com>
2021-01-24 19:11:22 +0530

    laserattack

---
+++ GameJam/Assets/bosslaserattack/laserdamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserdamage : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+        {
+            if (player.GetComponent<playerhealth>().phase == false)
+            {
+                player.GetComponent<playerhealth>().health -= 1;
+                player.GetComponent<playerhealth>().phase = true;
+            }
+
+        }
+    }
+}
